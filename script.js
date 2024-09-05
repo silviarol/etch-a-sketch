3c74ec95e9386fc88188a9c03f0f40c9d328c04a
silviarol <171265625+silviarol@users.noreply.github.com>
2024-09-05 14:20:34 +0200

    Create grid and hover effect

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const container = document.querySelector(".container");
 
-const divOne = document.createElement("div");
-divOne.style.backgroundColor = "white";
-divOne.style.border = "solid black 3px";
-divOne.style.width = "40px";
-divOne.style.height = "40px";
+for(let i = 0; i <=256; i++) {
 
-const divTwo = document.createElement("div");
-divTwo.style.backgroundColor = "white";
-divTwo.style.border = "solid black 3px";
-divTwo.style.width = "40px";
-divTwo.style.height = "40px";
+    const div = document.createElement("div");
+    div.style.width = "40px";
+    div.style.height = "40px";
+    div.style.border = "solid black 2px"
+    div.classList.add("grid");
+    container.appendChild(div);
 
-container.appendChild(divOne);
-container.appendChild(divTwo);
+}
