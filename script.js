2c4161a0f7473f15fd647e3a67d26556429bd8ab
silviarol <171265625+silviarol@users.noreply.github.com>
2024-09-05 16:11:40 +0200

    Fix grid size and add button

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i <257; i++) {
+for(let i = 0; i < 256; i++) {
 
     const div = document.createElement("div");
-    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px"; 
+    div.style.cssText = "border: 1px solid black; height: 60px; width: 60px"; 
     div.classList.add("grid");
     container.appendChild(div);
 
